7634dfeeeab1570ad7f4083e3e381a018e0d22e3
Lucas <lucass@gmail.com>
2011-10-04 10:03:49 -0300

    Google hide searchbar

---
+++ googleIgTweaks.user.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name           GoogleIG tweaks
+// @description    GoogleIG tweaks
+// @version        0.1
+// @author         Beothorn
+// @namespace      http://www.google.com/ig*
+// @include        http://www.google.com/ig*
+// @require       http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
+// ==/UserScript==
+  $("#nhdrwrap").remove();
