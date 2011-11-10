59b103c36995af01304421bf8864f96c78a0c72a
Lucas <lcsantos@wkcb-lcsantos.(none)>
2011-11-10 16:44:45 -0200

    google ig tweak now works on www.google.com.br

--- googleIgTweaks.user.js
+++ googleIgTweaks.user.js
@@ -3,8 +3,8 @@
 // @description    GoogleIG tweaks
 // @version        0.1
 // @author         Beothorn
-// @namespace      http://www.google.com/ig*
-// @include        http://www.google.com/ig*
+// @namespace      http://www.google.com.*/ig*
+// @include        http://www.google.com.*/ig*
 // @require       http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js
 // ==/UserScript==
   $("#nhdrwrap").remove();
